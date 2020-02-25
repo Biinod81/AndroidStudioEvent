49ac29f736dd000d1b8ba6045cac388256d11ab4
r-info-p222 <r-info-p222@iut-tlse3.fr>
2020-02-25 12:20:47 +0100

    TP2 event - calculatrice

--- app/src/main/java/com/lythomas/eventapplication/MainActivity.java
+++ app/src/main/java/com/lythomas/eventapplication/MainActivity.java
@@ -6,7 +6,11 @@ import android.os.Bundle;
 import android.text.Layout;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,10 +22,46 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.layout);
     }
 
-    public void onClick(View v){
+    public void onClickTEST(View v){
         TextView text = findViewById(R.id.textView2);
+        EditText op1 = findViewById(R.id.operande1);
+        EditText op2 = findViewById(R.id.operande2);
         Button b = (Button)v;
-        text.setText(b.getText().toString());
+        if (b.getText().toString()=="+"){
+            Toast.makeText(getApplicationContext(),"Addition",Toast.LENGTH_SHORT).show();
+            int res = Integer.parseInt(op1.getText().toString()) + Integer.parseInt(op2.getText().toString());
+            text.setText(res);
+        }
+    }
+
+    public void onClick(View v){
+        int res = 0;
+        TextView text = findViewById(R.id.textView2);
+        EditText op1 = findViewById(R.id.operande1);
+        EditText op2 = findViewById(R.id.operande2);
+        Button b = (Button)v;
+        switch (b.getId()){
+            case R.id.b1 :
+                res = Integer.parseInt(op1.getText().toString()) + Integer.parseInt(op2.getText().toString());;
+                text.setText("Résultat = "+res);
+                break;
+            case R.id.b2 :
+                res = Integer.parseInt(op1.getText().toString()) - Integer.parseInt(op2.getText().toString());
+                text.setText("Résultat = "+res);
+                break;
+            case R.id.b3 :
+                if (op2.getText().toString()=="0"){
+                    text.setText("Résultat = +/- infini");
+                    break;
+                }
+                //res = Integer.parseInt(op1.getText().toString()) / Integer.parseInt(op2.getText().toString());
+                //text.setText("Résultat = "+res);
+                break;
+            case R.id.b4 :
+                res = Integer.parseInt(op1.getText().toString()) * Integer.parseInt(op2.getText().toString());
+                text.setText("Résultat = "+res);
+                break;
+        }
     }
 
     public void onClickCompteur(View v){
